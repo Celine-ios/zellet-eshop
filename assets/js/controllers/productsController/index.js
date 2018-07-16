326b9199d0ca8f8653a6585081c366f4f863842f
wh0am123 <thejyjco@gmail.com>
2018-07-16 11:13:39 -0430

    Juan was here

--- assets/js/controllers/productsController/index.js
+++ assets/js/controllers/productsController/index.js
@@ -1,7 +1,39 @@
 app.controller('productsControl', function($scope, $http) {
+	//$http.get();
 	$scope.products = [{
-		greet: 'Hello'
+		name: 'Tal producto',
+		price: '19',
+		bs_price: '220000',
+		img: 'https://qph.fs.quoracdn.net/main-qimg-16b8fe5f4274e2baf68c2b226a17b2c5'
 	},{
-		greet: 'Bye'
+		name: 'Tal producto 2',
+		price: '32',
+		bs_price: '440000',
+		img: 'https://qph.fs.quoracdn.net/main-qimg-16b8fe5f4274e2baf68c2b226a17b2c5'
+	}, {
+		name: 'Tal producto 2',
+		price: '32',
+		bs_price: '440000',
+		img: 'https://qph.fs.quoracdn.net/main-qimg-16b8fe5f4274e2baf68c2b226a17b2c5'
+	}, {
+		name: 'Tal producto 2',
+		price: '32',
+		bs_price: '440000',
+		img: 'https://qph.fs.quoracdn.net/main-qimg-16b8fe5f4274e2baf68c2b226a17b2c5'
+	}, {
+		name: 'Tal producto 2',
+		price: '32',
+		bs_price: '440000',
+		img: 'https://qph.fs.quoracdn.net/main-qimg-16b8fe5f4274e2baf68c2b226a17b2c5'
+	}, {
+		name: 'Tal producto 2',
+		price: '32',
+		bs_price: '440000',
+		img: 'https://qph.fs.quoracdn.net/main-qimg-16b8fe5f4274e2baf68c2b226a17b2c5'
+	}, {
+		name: 'Tal producto 2',
+		price: '32',
+		bs_price: '440000',
+		img: 'https://qph.fs.quoracdn.net/main-qimg-16b8fe5f4274e2baf68c2b226a17b2c5'
 	}];
 });
---
+++ assets/js/directives/app-footer/index.js
@@ -0,0 +1,7 @@
+app.directive('appFooter', function() {
+	return {
+		restrict: 'E',
+		scope: {},
+		templateUrl: 'assets/templates/footer/index.html'
+	}
+});
